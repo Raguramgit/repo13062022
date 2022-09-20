9b1abed7ca9602613f0cf6c4905119372868972d
Raguraman01 <raguram7393@gmail.com>
2022-09-20 11:08:52 +0200

    First commit

---
+++ Test.py
@@ -0,0 +1,11 @@
+# My Test coding Documents
+#print ("hello world")
+#print (int(1.23))
+print ("Enter a value")
+a=int(input())
+print ("value of a =",a)
+print ("Enter a value")
+b=int(input())
+print ("value of b =",b)
+c=a+b
+print ("value of c =",c)
