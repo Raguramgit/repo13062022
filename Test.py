04a839ccc3c57d57efc6f55f71b23d10fc37c5b1
Raguraman01 <raguram7393@gmail.com>
2022-09-20 11:12:50 +0200

    Second commit

--- Test.py
+++ Test.py
@@ -1,8 +1,16 @@
 # My Test coding Documents
 #print ("hello world")
 #print (int(1.23))
-print ("Enter a value")
+"""print ("Enter a value")
 a=int(input())
+print ("value of a =",a)
+print ("Enter a value")
+b=int(input())
+print ("value of b =",b)
+c=a+b
+print ("value of c =",c)"""
+print ("Enter a value")
+a=float(input())
 print ("value of a =",a)
 print ("Enter a value")
 b=int(input())
