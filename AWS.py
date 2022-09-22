623463a95b0ac48701bdbfbe47821385e92fe8fb
Raguraman01 <raguram7393@gmail.com>
2022-09-22 12:31:56 +0200

    Create IAM User

--- AWS.py
+++ AWS.py
@@ -1,1 +1,14 @@
 #AWS Boto3 codes
+import boto3
+iam = boto3.resource('iam')
+user = iam.User('RAM')
+user = user.create(
+    Path='/',
+    PermissionsBoundary='arn:aws:iam::aws:policy/AdministratorAccess',
+    Tags=[
+        {
+            'Key': 'Department',
+            'Value': 'IT'
+        },
+    ]
+)
