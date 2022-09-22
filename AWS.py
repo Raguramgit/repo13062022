39f7fe7cbed0c3fe98f64300653416979c7fae74
Raguraman01 <raguram7393@gmail.com>
2022-09-22 13:21:28 +0200

    S3 Bucket creation

--- AWS.py
+++ AWS.py
@@ -1,5 +1,5 @@
 #AWS Boto3 codes
-import boto3
+"""import boto3
 iam = boto3.resource('iam')
 user = iam.User('RAM')
 user = user.create(
@@ -11,4 +11,15 @@ user = user.create(
             'Value': 'IT'
         },
     ]
+)"""
+import boto3
+s3 = boto3.resource('s3')
+bucket = s3.Bucket('ram22092022')
+response = bucket.create(
+    ACL='private',
+    CreateBucketConfiguration={
+        'LocationConstraint':'ap-southeast-1'
+    },
+    ObjectLockEnabledForBucket=False,
+    ObjectOwnership='BucketOwnerPreferred'
 )
